e2018e080ccd8ec34e80a4fa13681f66bdb094ac
Minh <minh@gmail.com>
2022-12-19 23:29:24 +0700

    first commit

---
+++ problem22.py
@@ -0,0 +1,37 @@
+#Find k’th node from the end of a linked list
+
+class Node :
+    def __init__(self,new_data):
+        self.data = new_data 
+        self.next = None 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def push(self,new_data):
+        new_node = Node(new_data)
+        new_node.next=self.head
+        self.head=new_node
+    def printNthfromLast(self, n):
+        temp = self.head
+
+        length = 0
+
+        while temp is not None:
+            temp = temp.next
+            length = length+1
+        if n>length:
+            print("Error")
+            return
+        temp = self.head
+        for i in range(0, length-n):
+            temp=temp.next
+        print(temp.data)
+
+
+if __name__ =="__main__" :
+    llist = LinkedList()
+    llist.push(20)
+    llist.push(4)
+    llist.push(15)
+    llist.push(35)
+    llist.printNthfromLast(4)
